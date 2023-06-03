2a7ee1b250e281b125570a047bd5ef15a56e2fbd
Claudia <135162896+claudiad4@users.noreply.github.com>
2023-06-03 19:45:17 +0200

    Add files via upload

---
+++ 4. Algorytmy_rozgałęzione_4/Algorytmy_rozgałęzione_4/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+public class MyProgram
+{
+    public static void Main(string[] args)
+    {
+        double a, b, c, d, licznik, mianownik;
+
+        Console.WriteLine("Podaj wartość liczby a:");
+        a = inputValue();
+        Console.WriteLine("Podaj wartość liczby b:");
+        b = inputValue();
+        Console.WriteLine("Podaj wartość liczby c:");
+        c = inputValue();
+        Console.WriteLine("Podaj wartość liczby d:");
+        d = inputValue();
+        if (b == 0 || d == 0)
+        {
+            Console.WriteLine("Brak rozwiązania.");
+        }
+        else
+        {
+            licznik = a * d + b * c;
+            mianownik = b * d;
+            Console.WriteLine("Licznik (x)=" + licznik);
+            Console.WriteLine("Mianownik (y)=" + mianownik);
+        }
+    }
+
+    // .NET can only read single characters or entire lines from the
+    // console. The following function safely reads a double value.
+    private static double inputValue()
+    {
+        double result;
+        while (!double.TryParse(Console.ReadLine(), out result)) ;
+        return result;
+    }
+}
---
+++ 5. Algorytmy_rozgałęzione_5/Algorytmy_rozgałęzione_5/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+public class MyProgram
+{
+    public static void Main(string[] args)
+    {
+        double a, b;
+
+        Console.WriteLine("Podaj dowolną liczbę:");
+        a = inputValue();
+        Console.WriteLine("Podaj liczbę, z którą chcesz porównać poprzednią liczbę:");
+        b = inputValue();
+        if (a == b)
+        {
+            Console.WriteLine("Te liczby są sobie równe!");
+        }
+        else
+        if (a > b)
+        {
+            Console.WriteLine("Z dwóch podanych liczb, większa jest liczba:" + a);
+        }
+        else
+        {
+            Console.WriteLine("Z dwóch podanych liczb, większa jest liczba:" + b);
+        }
+    }
+
+    // .NET can only read single characters or entire lines from the
+    // console. The following function safely reads a double value.
+    private static double inputValue()
+    {
+        double result;
+        while (!double.TryParse(Console.ReadLine(), out result)) ;
+        return result;
+    }
+}
+
+
