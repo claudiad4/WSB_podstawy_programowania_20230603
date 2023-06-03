a96c145742802b2b7228842b68d14852912c1421
Claudia <135162896+claudiad4@users.noreply.github.com>
2023-06-03 19:44:05 +0200

    Add files via upload

---
+++ 2. Algorytmy_sekwencyjne_2/Algorytmy_sekwencyjne_2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Obliczanie obwodu koła");
+
+        Console.Write("Podaj pole koła: ");
+        double pole = double.Parse(Console.ReadLine());
+
+        double promien = Math.Sqrt(pole / Math.PI);
+        double obwod = 2 * Math.PI * promien;
+
+        Console.WriteLine($"Obwód koła wynosi: {obwod}");
+    }
+}
---
+++ 3. Algorytmy_sekwencyjne_3/Algorytmy_sekwencyjne_3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public class MyProgram
+{
+    public static void Main(string[] args)
+    {
+        double waga, wzrost;
+
+        Console.WriteLine("Proszę podać swoją wagę w kilogramach:");
+        waga = inputValue();
+        Console.WriteLine("Proszę podać swój wzrost w metrach:");
+        wzrost = inputValue();
+        double bMI;
+
+        bMI = waga / (wzrost * wzrost);
+        double roundedBMI = Math.Round(bMI, 2);
+        Console.WriteLine("Twoje BMI wynosi:" + roundedBMI);
+    }
+
+    // .NET can only read single characters or entire lines from the
+    // console. The following function safely reads a double value.
+    private static double inputValue()
+    {
+        double result;
+        while (!double.TryParse(Console.ReadLine(), out result)) ;
+        return result;
+    }
+}
